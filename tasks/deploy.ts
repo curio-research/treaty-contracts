84c2a6ea512a5a1a2ca197a11f9926c90b5a49e6
Brandon Zhu <brandon.zhu@yale.edu>
2022-03-19 11:40:20 -0700

    multiple deployments

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -4,16 +4,25 @@ import { Epoch } from "./../typechain-types/Epoch";
 import { task } from "hardhat/config";
 import * as path from "path";
 import * as fsPromise from "fs/promises";
+import * as fs from "fs";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { deployProxy, printDivider } from "./util/deployHelper";
-import { LOCALHOST_RPC_URL, LOCALHOST_WS_RPC_URL, MAP_INTERVAL, masterItems, WORLD_HEIGHT, WORLD_WIDTH } from "./util/constants";
+import {
+  LOCALHOST_RPC_URL,
+  LOCALHOST_WS_RPC_URL,
+  MAP_INTERVAL,
+  masterItems,
+  WORLD_HEIGHT,
+  WORLD_WIDTH,
+} from "./util/constants";
 import { generateAllGameArgs } from "./util/allArgsGenerator";
 import { Getters, Game, GameStorage, Helper } from "../typechain-types";
 import { TowerGame } from "./../typechain-types/TowerGame";
 import { Permissions } from "../typechain-types";
 import { position } from "../util/types/common";
 import { visualizeMap } from "./util/mapGenerator";
+import { config } from "dotenv";
 
 // ---------------------------------
 // deploy script
@@ -22,8 +31,10 @@ import { visualizeMap } from "./util/mapGenerator";
 
 task("deploy", "deploy contracts")
   .addFlag("noport", "Don't port files to frontend") // default is to call port
+  .addFlag("publish", "Publish deployment to game launcher") // default is to call port
   .setAction(async (args: any, hre: HardhatRuntimeEnvironment) => {
-    const isDev = hre.network.name === "localhost" || hre.network.name === "hardhat";
+    const isDev =
+      hre.network.name === "localhost" || hre.network.name === "hardhat";
     await hre.run("compile");
 
     let player1: SignerWithAddress;
@@ -42,26 +53,59 @@ task("deploy", "deploy contracts")
     // initialize contracts
     const GameHelper = await deployProxy<Helper>("Helper", player1, hre, []);
     console.log("✦ GameHelper deployed");
-    const Permissions = await deployProxy<Permissions>("Permissions", player1, hre, [player1.address]);
+    const Permissions = await deployProxy<Permissions>(
+      "Permissions",
+      player1,
+      hre,
+      [player1.address]
+    );
     console.log("✦ Permissions deployed");
-    const GameStorage = await deployProxy<GameStorage>("GameStorage", player1, hre, [Permissions.address]);
+    const GameStorage = await deployProxy<GameStorage>(
+      "GameStorage",
+      player1,
+      hre,
+      [Permissions.address]
+    );
     console.log("✦ GameStorage deployed");
-    const GameContract = await deployProxy<Game>("Game", player1, hre, [...allGameArgs.gameDeployArgs, GameStorage.address, Permissions.address]);
+    const GameContract = await deployProxy<Game>("Game", player1, hre, [
+      ...allGameArgs.gameDeployArgs,
+      GameStorage.address,
+      Permissions.address,
+    ]);
     console.log("✦ GameContract deployed");
-    const TowerContract = await deployProxy<TowerGame>("TowerGame", player1, hre, [GameStorage.address, Permissions.address], { Helper: GameHelper.address });
+    const TowerContract = await deployProxy<TowerGame>(
+      "TowerGame",
+      player1,
+      hre,
+      [GameStorage.address, Permissions.address],
+      { Helper: GameHelper.address }
+    );
     console.log("✦ TowerContract deployed");
-    const GettersContract = await deployProxy<Getters>("Getters", player1, hre, [GameContract.address, GameStorage.address]);
+    const GettersContract = await deployProxy<Getters>(
+      "Getters",
+      player1,
+      hre,
+      [GameContract.address, GameStorage.address]
+    );
     console.log("✦ GettersContract deployed");
     const EpochContract = await deployProxy<Epoch>("Epoch", player1, hre, [10]);
     console.log("✦ EpochContract deployed");
 
-    const GET_MAP_INTERVAL = (await GettersContract.GET_MAP_INTERVAL()).toNumber();
+    const GET_MAP_INTERVAL = (
+      await GettersContract.GET_MAP_INTERVAL()
+    ).toNumber();
 
     // add contract permissions
-    const p1tx = await Permissions.connect(player1).setPermission(GameContract.address, true);
+    const p1tx = await Permissions.connect(player1).setPermission(
+      GameContract.address,
+      true
+    );
     p1tx.wait();
 
-    const p2tx = await Permissions.connect(player1).setPermission(TowerContract.address, true);
+    const p2tx = await Permissions.connect(player1).setPermission(
+      TowerContract.address,
+      true
+    );
     p2tx.wait();
 
     // make this into a table
@@ -80,7 +124,9 @@ task("deploy", "deploy contracts")
     let regionMap: number[][][];
     for (let x = 0; x < WORLD_WIDTH; x += MAP_INTERVAL) {
       for (let y = 0; y < WORLD_HEIGHT; y += MAP_INTERVAL) {
-        regionMap = blocks.slice(x, x + MAP_INTERVAL).map((col) => col.slice(y, y + MAP_INTERVAL));
+        regionMap = blocks
+          .slice(x, x + MAP_INTERVAL)
+          .map((col) => col.slice(y, y + MAP_INTERVAL));
 
         let tx = await GameStorage._setMapRegion({ x, y }, regionMap);
         tx.wait();
@@ -110,18 +156,22 @@ task("deploy", "deploy contracts")
       let tx;
 
       tx = await GameContract.connect(player1).initializePlayer(player1Pos); // initialize users
-      await tx.wait();
+      tx.wait();
 
       tx = await GameContract.connect(player2).initializePlayer(player2Pos);
-      await tx.wait();
+      tx.wait();
 
-      tx = await GameStorage.connect(player1)._increaseItemInInventory(player1.address, 0, 100);
-      await tx.wait();
-
-      console.log("✦ setting epoch controller");
-      tx = await GameStorage.setEpochController(EpochContract.address); // set epoch controller
-      await tx.wait();
+      tx = await GameStorage.connect(player1)._increaseItemInInventory(
+        player1.address,
+        0,
+        100
+      );
+      tx.wait();
     }
+
+    console.log("✦ setting epoch controller");
+    const tx = await GameStorage.setEpochController(EpochContract.address); // set epoch controller
+    tx.wait();
 
     // bulk initialize towers
     console.log("✦ initializing towers");
@@ -132,7 +182,10 @@ task("deploy", "deploy contracts")
       allTowers.push(tower.tower);
     }
 
-    const towerTx = await TowerContract.addTowerBulk(allTowerLocations, allTowers);
+    const towerTx = await TowerContract.addTowerBulk(
+      allTowerLocations,
+      allTowers
+    );
     await towerTx.wait();
 
     // ---------------------------------
@@ -155,12 +208,44 @@ task("deploy", "deploy contracts")
       GET_MAP_INTERVAL: GET_MAP_INTERVAL,
       BLOCK_ID_TO_NAME_MAP: generateBlockIdToNameMap(masterItems),
     };
+    /*
+    await fsPromise.writeFile(
+      path.join(currentFileDir, "game.config.json"),
+      JSON.stringify(configFile)
+    );*/
 
-    await fsPromise.writeFile(path.join(currentFileDir, "game.config.json"), JSON.stringify(configFile));
+    const publish = args.publish;
+    if (publish) {
+      //send shit to mongo
+    }
 
-    const noPort = args.noport; // port flag
-    if (noPort) return;
-    await hre.run("port"); // default to porting files
+    if (isDev) {
+      const configFileDir = path.join(currentFileDir, "game.config.json");
+
+      const existingDeployments = await fs
+        .readFileSync(configFileDir)
+        .toString();
+
+      console.log(existingDeployments.length);
+
+      if (existingDeployments.length == 0) {
+        await fsPromise.writeFile(
+          configFileDir,
+          "[" + JSON.stringify(configFile) + "]"
+        );
+      } else {
+        await fsPromise.writeFile(
+          configFileDir,
+          "[" +
+            existingDeployments.substring(1, existingDeployments.length - 2) +
+            "," +
+            JSON.stringify(configFile) +
+            "]"
+        );
+      }
+
+      await hre.run("port"); // default to porting files
+    }
   });
 
 export const rpcUrlSelector = (networkName: string): string[] => {
