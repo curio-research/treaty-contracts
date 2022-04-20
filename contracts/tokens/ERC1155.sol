9676d648d3f70d1ca05105c0b167cbc4879a09de
Brandon Zhu <brandon.zhu@yale.edu>
2022-04-20 02:04:03 -0400

    finished ERC1155 implementation

--- contracts/tokens/ERC1155.sol
+++ contracts/tokens/ERC1155.sol
@@ -18,7 +18,7 @@ contract ERC1155 is IERC1155 {
     bytes4 internal constant ERC1155_BATCH_RECEIVED_VALUE = 0xbc197c81;
 
     // id => (owner => balance)
-    mapping(address => mapping(uint256 => uint256)) internal balances;
+    mapping(uint256 => mapping(address => uint256)) internal balances;
 
     // owner => (operator => approved)
     mapping(address => mapping(address => bool)) internal operatorApproval;
@@ -41,11 +41,6 @@ contract ERC1155 is IERC1155 {
      */
     bytes4 private constant INTERFACE_SIGNATURE_ERC1155 = 0xd9b67a26;
 
-    /**
-     * @notice Query if a contract implements an interface
-     * @param _interfaceID  The interface identifier, as specified in ERC-165
-     * @return `true` if the contract implements `_interfaceID` and
-     */
     function supportsInterface(bytes4 _interfaceId) public view returns (bool) {
         if (_interfaceId == INTERFACE_SIGNATURE_ERC165 || _interfaceId == INTERFACE_SIGNATURE_ERC1155) {
             return true;
@@ -79,6 +74,71 @@ contract ERC1155 is IERC1155 {
             _doSafeTransferAcceptanceCheck(msg.sender, _from, _to, _id, _value, _data);
         }
     }
+
+    function safeBatchTransferFrom(
+        address _from,
+        address _to,
+        uint256[] calldata _ids,
+        uint256[] calldata _values,
+        bytes calldata _data
+    ) external {
+        // MUST Throw on errors
+        require(_to != address(0x0), "destination address must be non-zero.");
+        require(_ids.length == _values.length, "_ids and _values array length must match.");
+        require(_from == msg.sender || operatorApproval[_from][msg.sender] == true, "Need operator approval for 3rd party transfers.");
+
+        for (uint256 i = 0; i < _ids.length; ++i) {
+            uint256 id = _ids[i];
+            uint256 value = _values[i];
+
+            // SafeMath will throw with insuficient funds _from
+            // or if _id is not valid (balance will be 0)
+            balances[id][_from] = balances[id][_from].sub(value);
+            balances[id][_to] = value.add(balances[id][_to]);
+        }
+
+        // MUST emit event
+        emit TransferBatch(msg.sender, _from, _to, _ids, _values);
+
+        // Now that the balances are updated and the events are emitted,
+        // call onERC1155BatchReceived if the destination is a contract.
+        if (_to.isContract()) {
+            _doSafeBatchTransferAcceptanceCheck(msg.sender, _from, _to, _ids, _values, _data);
+        }
+    }
+
+    /***********************************|
+  |         Balance Functions         |
+  |__________________________________*/
+
+    function balanceOf(address _owner, uint256 _id) external view returns (uint256) {
+        return balances[_id][_owner];
+    }
+
+    function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids) external view returns (uint256[] memory) {
+        require(_owners.length == _ids.length);
+
+        uint256[] memory batchBalances = new uint256[](_owners.length);
+
+        for (uint256 i = 0; i < _owners.length; ++i) {
+            batchBalances[i] = balances[_ids[i]][_owners[i]];
+        }
+
+        return batchBalances;
+    }
+
+    function setApprovalForAll(address _operator, bool _approved) external {
+        operatorApproval[msg.sender][_operator] = _approved;
+        emit ApprovalForAll(msg.sender, _operator, _approved);
+    }
+
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool) {
+        return operatorApproval[_owner][_operator];
+    }
+
+    /***********************************|
+  |         Transfer Acceptance Check   |
+  |__________________________________*/
 
     function _doSafeTransferAcceptanceCheck(
         address _operator,
