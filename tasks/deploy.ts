8f7a2290a71b3c35a5325f722eea9f2fda27a8f1
Yijia-Chen <yijiachenmain@gmail.com>
2022-06-16 14:26:30 -0700

    chore: deterministic map for client

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -6,7 +6,7 @@ import { Util } from './../typechain-types/Util';
 import { task } from 'hardhat/config';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { deployProxy, printDivider } from './util/deployHelper';
-import { TROOP_TYPES, getTroopTypeIndexByName, RENDER_CONSTANTS, MAP_INTERVAL, NUM_CITIES, NUM_PORTS, SECONDS_PER_EPOCH, WORLD_HEIGHT, WORLD_WIDTH, getTroopNames, generateWorldConstants } from './util/constants';
+import { TROOP_TYPES, getTroopTypeIndexByName, RENDER_CONSTANTS, MAP_INTERVAL, NUM_CITIES, NUM_PORTS, SECONDS_PER_EPOCH, WORLD_HEIGHT, WORLD_WIDTH, getTroopNames, generateWorldConstants, ligmap } from './util/constants';
 import { position } from '../util/types/common';
 import { deployDiamond, deployFacets, getDiamond } from './util/diamondDeploy';
 import { MapInput, TILE_TYPE, TROOP_NAME } from './util/types';
@@ -22,6 +22,7 @@ const { BACKEND_URL } = process.env;
 task('deploy', 'deploy contracts')
   .addFlag('noport', "Don't port files to frontend") // default is to call port
   .addFlag('publish', 'Publish deployment to game launcher') // default is to call publish
+  .addFlag('fixmap', 'Use deterministic map') // default is non-deterministic maps
   .setAction(async (args: any, hre: HardhatRuntimeEnvironment) => {
     await hre.run('compile');
     printDivider();
@@ -29,21 +30,29 @@ task('deploy', 'deploy contracts')
     const isDev = hre.network.name === 'localhost' || hre.network.name === 'hardhat';
     console.log('Network:', hre.network.name);
 
+    const fixMap = args.fixmap;
+    if (fixMap) console.log('Using deterministic map');
+
     let [player1, player2] = await hre.ethers.getSigners();
-    const armyTypeId = getTroopTypeIndexByName(TROOP_TYPES, TROOP_NAME.ARMY) + 1;
+    const armyTroopTypeId = getTroopTypeIndexByName(TROOP_TYPES, TROOP_NAME.ARMY) + 1;
+    const troopTransportTroopTypeId = getTroopTypeIndexByName(TROOP_TYPES, TROOP_NAME.TROOP_TRANSPORT) + 1;
+    const destroyerTroopTypeId = getTroopTypeIndexByName(TROOP_TYPES, TROOP_NAME.DESTROYER) + 1;
 
     // Set up game configs
     const worldConstants = generateWorldConstants(player1.address);
 
-    const { tileMap, colorMap } = generateGameMaps(
-      {
-        width: WORLD_WIDTH,
-        height: WORLD_HEIGHT,
-        numPorts: NUM_PORTS,
-        numCities: NUM_CITIES,
-      } as MapInput,
-      RENDER_CONSTANTS
-    );
+    let tileMap: TILE_TYPE[][];
+    tileMap = fixMap
+      ? ligmap
+      : (tileMap = generateGameMaps(
+          {
+            width: WORLD_WIDTH,
+            height: WORLD_HEIGHT,
+            numPorts: NUM_PORTS,
+            numCities: NUM_CITIES,
+          } as MapInput,
+          RENDER_CONSTANTS
+        ).tileMap);
 
     // Deploy util contracts
     const util = await deployProxy<Util>('Util', player1, hre, []);
@@ -78,35 +87,58 @@ task('deploy', 'deploy contracts')
       let x: number;
       let y: number;
 
-      let player1Pos: position;
-      do {
-        x = Math.floor(Math.random() * WORLD_WIDTH);
-        y = Math.floor(Math.random() * WORLD_HEIGHT);
-        player1Pos = { x, y };
-      } while (tileMap[x][y] != TILE_TYPE.PORT);
+      if (fixMap) {
+        const player1Pos = { x: 2, y: 4 };
+        const player2Pos = { x: 4, y: 2 };
+        const player1ArmyPos = { x: 3, y: 3 };
+        const player2ArmyPos = { x: 3, y: 2 };
+        const player1TroopTransportPos = { x: 5, y: 3 };
+        const player2DestroyerPos = { x: 5, y: 4 };
 
-      let player2Pos: position;
-      do {
-        x = Math.floor(Math.random() * WORLD_WIDTH);
-        y = Math.floor(Math.random() * WORLD_HEIGHT);
-        player2Pos = { x, y };
-      } while (tileMap[x][y] != TILE_TYPE.PORT && player2Pos.x !== player1Pos.x && player2Pos.y !== player1Pos.y);
+        let tx;
+        tx = await diamond.connect(player1).initializePlayer(player1Pos, player1.address);
+        await tx.wait();
+        tx = await diamond.connect(player1).initializePlayer(player2Pos, player2.address);
+        await tx.wait();
+        tx = await diamond.connect(player1).spawnTroop(player1ArmyPos, player1.address, armyTroopTypeId);
+        await tx.wait();
+        tx = await diamond.connect(player1).spawnTroop(player2ArmyPos, player2.address, armyTroopTypeId);
+        await tx.wait();
+        tx = await diamond.connect(player1).spawnTroop(player1TroopTransportPos, player1.address, troopTransportTroopTypeId);
+        await tx.wait();
+        tx = await diamond.connect(player1).spawnTroop(player2DestroyerPos, player2.address, destroyerTroopTypeId);
+        await tx.wait();
+      } else {
+        let player1Pos: position;
+        let player2Pos: position;
+        do {
+          x = Math.floor(Math.random() * WORLD_WIDTH);
+          y = Math.floor(Math.random() * WORLD_HEIGHT);
+          player1Pos = { x, y };
+        } while (tileMap[x][y] != TILE_TYPE.PORT);
 
-      // Give each player a port and an army to start with
-      let tx = await diamond.connect(player1).initializePlayer(player1Pos, player1.address);
-      await tx.wait();
-      tx = await diamond.connect(player1).initializePlayer(player2Pos, player2.address);
-      await tx.wait();
-      tx = await diamond.connect(player1).spawnTroop(player1Pos, player1.address, armyTypeId);
-      await tx.wait();
-      tx = await diamond.connect(player1).spawnTroop(player2Pos, player2.address, armyTypeId);
-      await tx.wait();
+        do {
+          x = Math.floor(Math.random() * WORLD_WIDTH);
+          y = Math.floor(Math.random() * WORLD_HEIGHT);
+          player2Pos = { x, y };
+        } while (tileMap[x][y] != TILE_TYPE.PORT && player2Pos.x !== player1Pos.x && player2Pos.y !== player1Pos.y);
 
-      // Basic checks
-      const player1Army = await diamond.getTroopAt(player1Pos);
-      if (player1Army.owner !== player1.address) throw new Error('Something is wrong');
-      const player2Army = await diamond.getTroopAt(player2Pos);
-      if (player2Army.troopTypeId.toNumber() !== armyTypeId) throw new Error('Something went wrong');
+        // Give each player a port and an army to start with
+        let tx = await diamond.connect(player1).initializePlayer(player1Pos, player1.address);
+        await tx.wait();
+        tx = await diamond.connect(player1).initializePlayer(player2Pos, player2.address);
+        await tx.wait();
+        tx = await diamond.connect(player1).spawnTroop(player1Pos, player1.address, armyTroopTypeId);
+        await tx.wait();
+        tx = await diamond.connect(player1).spawnTroop(player2Pos, player2.address, armyTroopTypeId);
+        await tx.wait();
+
+        // Basic checks
+        const player1Army = await diamond.getTroopAt(player1Pos);
+        if (player1Army.owner !== player1.address) throw new Error('Something is wrong');
+        const player2Army = await diamond.getTroopAt(player2Pos);
+        if (player2Army.troopTypeId.toNumber() !== armyTroopTypeId) throw new Error('Something went wrong');
+      }
     }
 
     // ---------------------------------
