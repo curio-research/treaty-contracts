057b56bab9ec995694f0f2d9803b563facb1b5bc
Yijia Chen <38818142+Yijia-Chen@users.noreply.github.com>
2022-07-07 17:47:38 -0700

    deploy script cleanup (#89)

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -6,13 +6,12 @@ import { Util } from './../typechain-types/Util';
 import { task } from 'hardhat/config';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { deployProxy, printDivider } from './util/deployHelper';
-import { TROOP_TYPES, getTroopTypeIndexByName, RENDER_CONSTANTS, NUM_CITIES, NUM_PORTS, WORLD_HEIGHT, WORLD_WIDTH, generateWorldConstants, ligmap } from './util/constants';
+import { TROOP_TYPES, getTroopTypeIndexByName, RENDER_CONSTANTS, NUM_CITIES, NUM_PORTS, WORLD_HEIGHT, WORLD_WIDTH, generateWorldConstants, ligmap, ligmapPortTiles, ligmapCityTiles } from './util/constants';
 import { position } from '../util/types/common';
 import { deployDiamond, deployFacets, getDiamond } from './util/diamondDeploy';
 import { MapInput, TILE_TYPE, TROOP_NAME } from './util/types';
 import { encodeTileMap, generateGameMaps } from './util/mapHelper';
 import { gameConfig } from '../api/types';
-import _ from 'lodash';
 
 // ---------------------------------
 // deploy script
@@ -22,51 +21,59 @@ import _ from 'lodash';
 task('deploy', 'deploy contracts')
   .addFlag('noport', "Don't port files to frontend") // default is to call port
   .addFlag('publish', 'Publish deployment to game launcher') // default is to call publish
-  .addFlag('fixmap', 'Use deterministic map') // default is non-deterministic maps
+  .addFlag('fixmap', 'Use deterministic map') // default is non-deterministic maps; deterministic maps are mainly used for client development
   .setAction(async (args: any, hre: HardhatRuntimeEnvironment) => {
     try {
+      // Compile contracts
       await hre.run('compile');
       printDivider();
 
+      // Read variables from run flags
       const isDev = hre.network.name === 'localhost' || hre.network.name === 'hardhat';
       console.log('Network:', hre.network.name);
+      const fixMap = args.fixmap;
+      if (fixMap) console.log('Using deterministic map');
+      const publish = args.publish;
 
-      // check connection with faucet to make sure deployment will post
+      // Check connection with faucet to make sure deployment will post
       if (!isDev) {
         await isConnectionLive();
       }
 
-      const fixMap = args.fixmap;
-      if (fixMap) console.log('Using deterministic map');
-
+      // Set up deployer and some local variables
       let [player1, player2] = await hre.ethers.getSigners();
       const armyTroopTypeId = getTroopTypeIndexByName(TROOP_TYPES, TROOP_NAME.ARMY) + 1;
       const troopTransportTroopTypeId = getTroopTypeIndexByName(TROOP_TYPES, TROOP_NAME.TROOP_TRANSPORT) + 1;
       const destroyerTroopTypeId = getTroopTypeIndexByName(TROOP_TYPES, TROOP_NAME.DESTROYER) + 1;
 
-      // Set up game configs
+      // Set up game configs and map
       const worldConstants = generateWorldConstants(player1.address);
 
-      let deployMap: TILE_TYPE[][];
-      const { tileMap, portTiles, cityTiles } = generateGameMaps(
-        {
-          width: WORLD_WIDTH,
-          height: WORLD_HEIGHT,
-          numPorts: NUM_PORTS,
-          numCities: NUM_CITIES,
-        } as MapInput,
-        RENDER_CONSTANTS
-      );
+      let tileMap: TILE_TYPE[][];
+      let portTiles: number[][];
+      let cityTiles: number[][];
 
       if (fixMap) {
-        deployMap = ligmap;
+        tileMap = ligmap;
+        portTiles = ligmapPortTiles;
+        cityTiles = ligmapCityTiles;
       } else {
-        deployMap = tileMap;
+        const gameMaps = generateGameMaps(
+          {
+            width: WORLD_WIDTH,
+            height: WORLD_HEIGHT,
+            numPorts: NUM_PORTS,
+            numCities: NUM_CITIES,
+          } as MapInput,
+          RENDER_CONSTANTS
+        );
+        tileMap = gameMaps.tileMap;
+        portTiles = gameMaps.portTiles;
+        cityTiles = gameMaps.cityTiles;
       }
 
       // Deploy util contracts
       const util = await deployProxy<Util>('Util', player1, hre, []);
-      // const marchHelper = await deployProxy<MarchHelper>('MarchHelper', player1, hre, []);
       console.log('✦ Util:', util.address);
 
       // Deploy diamond and facets
@@ -86,22 +93,23 @@ task('deploy', 'deploy contracts')
       const encodedTileMap = encodeTileMap(tileMap);
       await (await diamond.storeEncodedColumnBatches(encodedTileMap)).wait();
       const time2 = performance.now();
-      console.log(`✦ lazy set ${WORLD_WIDTH}x${WORLD_HEIGHT} map took ${time2 - time1} ms`);
+      console.log(`✦ lazy set ${tileMap.length}x${tileMap[0].length} map took ${time2 - time1} ms`);
 
+      // Initialize tiles with base
       const portTilePositions = portTiles.map((portTile) => ({ x: portTile[0], y: portTile[1] }));
       const cityTilePositions = cityTiles.map((cityTile) => ({ x: cityTile[0], y: cityTile[1] }));
 
-      // initialize bases
       console.log('✦ initializing bases');
       await (await diamond.bulkInitializeTiles([...portTilePositions, ...cityTilePositions])).wait();
 
+      // Randomly initialize players if on localhost
       if (isDev) {
-        // Randomly initialize players only if we're on localhost
         console.log('✦ initializing players');
         let x: number;
         let y: number;
 
         if (fixMap) {
+          // Primary setting for client development
           const player1Pos = { x: 2, y: 4 };
           const player2Pos = { x: 4, y: 2 };
           const player1ArmyPos = { x: 3, y: 3 };
@@ -115,8 +123,6 @@ task('deploy', 'deploy contracts')
 
           await (await diamond.connect(player1).initializePlayer(player1Pos, player1.address)).wait();
           await (await diamond.connect(player1).initializePlayer(player2Pos, player2.address)).wait();
-
-          // spawn testing troops
           await (await diamond.connect(player1).spawnTroop(player1ArmyPos, player1.address, armyTroopTypeId)).wait();
           await (await diamond.connect(player1).spawnTroop(player1ArmyPos2, player1.address, armyTroopTypeId)).wait();
           await (await diamond.connect(player1).spawnTroop(player1ArmyPos3, player1.address, armyTroopTypeId)).wait();
@@ -126,46 +132,36 @@ task('deploy', 'deploy contracts')
           await (await diamond.connect(player1).spawnTroop(player1TroopTransportPos, player1.address, troopTransportTroopTypeId)).wait();
           await (await diamond.connect(player1).spawnTroop(player2DestroyerPos, player2.address, destroyerTroopTypeId)).wait();
         } else {
+          // Primary setting for local playtesting
           let player1Pos: position;
           let player2Pos: position;
           do {
             x = Math.floor(Math.random() * WORLD_WIDTH);
             y = Math.floor(Math.random() * WORLD_HEIGHT);
             player1Pos = { x, y };
-          } while (deployMap[x][y] != TILE_TYPE.PORT);
+          } while (tileMap[x][y] != TILE_TYPE.PORT);
 
           do {
             x = Math.floor(Math.random() * WORLD_WIDTH);
             y = Math.floor(Math.random() * WORLD_HEIGHT);
             player2Pos = { x, y };
-          } while (deployMap[x][y] !== TILE_TYPE.PORT || player2Pos.x === player1Pos.x || player2Pos.y === player1Pos.y);
+          } while (tileMap[x][y] !== TILE_TYPE.PORT || player2Pos.x === player1Pos.x || player2Pos.y === player1Pos.y);
 
-          // Give each player a port and an army to start with
+          // Give each player a port to start with
           await (await diamond.connect(player1).initializePlayer(player1Pos, player1.address)).wait();
           await (await diamond.connect(player1).initializePlayer(player2Pos, player2.address)).wait();
         }
       }
 
-      // ---------------------------------
-      // generate config files
-      // copies files and ports to frontend if it's a localhost, or publishes globally if its a global deployment
-      // ---------------------------------
-
+      // Generate config files
       const configFile: gameConfig = {
         address: diamond.address,
         network: hre.network.name,
         deploymentId: `${hre.network.name}-${Date.now()}`,
-        map: deployMap,
+        map: tileMap,
       };
 
-      const publish = args.publish;
-
-      // publish deployment
-      if (publish || !isDev) {
-        await publishDeployment(configFile);
-      }
-
-      // If we're in dev mode, port the files to the frontend.
+      // Port files to frontend if on localhost
       if (isDev) {
         const configFileDir = path.join(path.join(__dirname), 'game.config.json');
         const raw = fs.readFileSync(configFileDir).toString();
@@ -174,6 +170,11 @@ task('deploy', 'deploy contracts')
 
         await fsPromise.writeFile(configFileDir, JSON.stringify(existingDeployments));
         await hre.run('port'); // default to porting files
+      }
+
+      // Publish deployment if on network
+      if (publish || !isDev) {
+        await publishDeployment(configFile);
       }
     } catch (err) {
       console.log(err);
