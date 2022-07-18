e0b63b222cdc673abc2484e7f649bf5e64f18b71
Yijia Chen <38818142+Yijia-Chen@users.noreply.github.com>
2022-07-18 00:02:11 -0700

    release: sandbox (#109)
    * chore: git actions port to public repo (#60)
    
    * (wip) feat: push to public repo hook
    
    * fix: test hook filename
    
    * add ssh deploy key
    
    * fix: every step must define a  or  key
    
    * try again
    
    * again?
    
    * Add foundry test
    
    * Remove hardaht test
    
    * Add yarn?
    
    * try something
    
    * change version
    
    * remove
    
    * add
    
    * try
    
    * pray to god
    
    * prayyy
    
    * try try
    
    * another one
    
    * this should fking work
    
    * ?????
    
    * bruh imma give up
    
    * man
    
    * 我好失望
    
    * sdfansf
    
    * fiqhwe
    
    * ferw
    
    * does dat work?
    
    * nvm
    
    * this shooooould work
    
    * some
    
    * finalize
    
    * change condition
    
    * 123
    
    * include foundry stuff
    
    * separate into 2 files
    
    * try
    
    * add build?
    
    * remove build
    
    * remove altogether
    
    Co-authored-by: curio-bot <yijia@0xcurio.com>
    Co-authored-by: Yijia-Chen <yijiachenmain@gmail.com>
    Co-authored-by: curio-bot <yijia@curio.gg>
    
    * Add getPlayerCount to getter (#108)
    
    * chore: final cleanup (#107)
    
    * lower init player balance
    
    * allow negative balances
    
    * restore mediterrainean original map
    
    * shell script for deploying multiple sandbox games
    
    * update READNE
    
    * add multiple map instruction
    
    * chore: pre-release cleanup script (#110)
    
    * chore: pre-release-cleanup
    
    * force remove
    
    * chore: remove PR trigger for port and other minor fix (#111)
    
    * chore: remove PR trigger for port and other minor fix
    
    * add initial gold
    
    * update constants
    
    Co-authored-by: Kevin Z <40219813+kevinz917@users.noreply.github.com>
    Co-authored-by: curio-bot <yijia@0xcurio.com>
    Co-authored-by: curio-bot <yijia@curio.gg>

---
+++ scripts/release.sh
@@ -0,0 +1,22 @@
+#/bin/zsh
+
+# save `.env`
+mv .env ../.env.saved
+
+# remove all files and directories specified in `.gitignore`
+git clean -fxd
+
+# create new `public` directory
+rm -rf public
+mkdir public
+
+# clone repo there and remove unnecessary files and directories
+cp -r . public
+cd public; rm -rf public; rm -rf .git; rm -rf .github; rm -rf scripts; rm deploy_sandbox.sh; cd ..
+
+# restore `.env`
+mv ../.env.saved .env
+
+# restore `node_modules` and `out`
+yarn
+yarn ft
