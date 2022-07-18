4ac9f50fed12bfcdab899376708d9af3ae5ec798
Yijia-Chen <yijiachenmain@gmail.com>
2022-07-17 23:42:31 -0700

    force remove

--- release.sh
+++ release.sh
@@ -7,12 +7,12 @@ mv .env ../.env.saved
 git clean -fxd
 
 # create new `public` directory
-rm -r public
+rm -rf public
 mkdir public
 
 # clone repo there and remove unnecessary files and directories
 cp -r . public
-cd public; rm -r public; rm -r .git; rm -r .github; rm release.sh; cd ..
+cd public; rm -rf public; rm -rf .git; rm -rf .github; rm release.sh; cd ..
 
 # restore `.env`
 mv ../.env.saved .env
