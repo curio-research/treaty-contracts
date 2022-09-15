17d78a3cb7277277f1d1d069ba696d4902f83018
ModeoC <63042678+ModeoC@users.noreply.github.com>
2022-09-15 11:31:56 -0700

    createTreaty

---
+++ contracts/facets/NATO.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NATO is Ownable {
+  uint256 private value;
+// Emitted when the stored value changes
+  event ValueChanged(uint256 newValue);
+// Stores a new value in the contract
+  function store(uint256 newValue) public onlyOwner {
+    value = newValue;
+    emit ValueChanged(newValue);
+  }
+// Reads the last stored value
+  function retrieve() public view returns (uint256) {
+    return value;
+  }
+}
