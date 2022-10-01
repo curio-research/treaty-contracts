7d4aedb12ddf9aad7b87b31640200fe324c0e09c
Kevin Z <zkw917@gmail.com>
2022-09-30 17:31:15 -0700

    Clean and delete ignores

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 forge build
 lsof -ti:8545 | xargs kill
-anvil --gas-limit 10000000000 --code-size-limit 1000000000 --base-fee 0 --gas-price 0 & npx hardhat deploy
+anvil --gas-limit 10000000000 --code-size-limit 1000000000 --base-fee 0 --gas-price 0 & npx hardhat deploy --fixmap
