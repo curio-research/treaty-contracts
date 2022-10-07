06c3595f89a91a37e09fa673b21e3efc9dd77abd
Kevin Z <zkw917@gmail.com>
2022-10-06 22:43:50 -0700

    Fix blockchain time out of sync problem finally holy fuck

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,11 @@
 forge build
 lsof -ti:8545 | xargs kill
-anvil --gas-limit 10000000000 --code-size-limit 100000000000 --base-fee 100 --gas-price 1000 & npx hardhat deploy 
+
+# unixTimeInSeconds=$(date +%s)
+# echo "$unixTimeInSeconds"
+
+# anvil --gas-limit 10000000000 --code-size-limit 100000000000 --base-fee 100 --gas-price 1000 --timestamp "$(($unixTimeInSeconds-150))" --silent & npx hardhat deploy
+
+
+anvil --gas-limit 10000000000 --code-size-limit 100000000000 --base-fee 100 --gas-price 1000 --timestamp 0 --silent & npx hardhat deploy
+
