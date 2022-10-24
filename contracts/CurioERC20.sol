9bf7bd9a3c50b22c319890fce9aa745e6af15c30
Yijia-Chen <yijiachenmain@gmail.com>
2022-10-24 11:31:07 -0700

    add tokenization

--- contracts/CurioERC20.sol
+++ contracts/CurioERC20.sol
@@ -1,40 +1,40 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-// import {ERC20} from "lib/solmate/src/tokens/ERC20.sol";
-// import {Position} from "contracts/libraries/Types.sol";
-// import {ECSLib} from "contracts/libraries/ECSLib.sol";
-// import {GameLib} from "contracts/libraries/GameLib.sol";
+import {ERC20} from "lib/solmate/src/tokens/ERC20.sol";
+import {Position} from "contracts/libraries/Types.sol";
+import {ECSLib} from "contracts/libraries/ECSLib.sol";
+import {GameLib} from "contracts/libraries/GameLib.sol";
 
-// contract CurioERC20 is ERC20 {
-//     /// Needed functions:
-//     /// - approve
-//     /// - transfer
-//     /// - transferFrom
-//     /// - permit
-//     /// - _mint (internal)
-//     /// - _burn (internal)
+contract CurioERC20 is ERC20 {
+    /// Needed functions:
+    /// - approve
+    /// - transfer
+    /// - transferFrom
+    /// - permit
+    /// - _mint (internal)
+    /// - _burn (internal)
 
-//     Position public position;
+    Position public position;
 
-//     constructor(
-//         string memory _name,
-//         string memory _symbol,
-//         uint8 _decimals
-//     ) ERC20(_name, _symbol, _decimals) {}
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol, _decimals) {}
 
-//     function approve(address spender, uint256 amount) public override returns (bool) {
-//         // TODO
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        // TODO
 
-//         return super.approve(spender, amount);
-//     }
+        return super.approve(spender, amount);
+    }
 
-//     function transfer(address to, uint256 amount) public override returns (bool) {
-//         return true;
-//     }
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        return true;
+    }
 
-//     function _canTransfer() private returns (bool) {
-//         // TODO
-//         return true;
-//     }
-// }
+    function _canTransfer() private returns (bool) {
+        // TODO
+        return true;
+    }
+}
