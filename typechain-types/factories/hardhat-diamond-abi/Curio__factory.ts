1ccaa69791f86789cf201f4ea34bc67b45a4a4cf
Kevin Z <40219813+kevinz917@users.noreply.github.com>
2022-10-26 15:42:46 -0700

    Bulk Harvest Resources (#201)
    * New
    
    * Remove unnecssary typechain files

--- typechain-types/factories/hardhat-diamond-abi/Curio__factory.ts
+++ typechain-types/factories/hardhat-diamond-abi/Curio__factory.ts
@@ -728,6 +728,19 @@ const _abi = [
   {
     inputs: [
       {
+        internalType: "uint256[]",
+        name: "resourceIds",
+        type: "uint256[]",
+      },
+    ],
+    name: "harvestResources",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
         internalType: "uint256",
         name: "_buildingID",
         type: "uint256",
