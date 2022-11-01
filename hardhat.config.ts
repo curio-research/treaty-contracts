3d0cfc7e550dd425dfbe8e1b85ae1fb8289948b4
Kevin Z <zkw917@gmail.com>
2022-10-31 17:52:13 -0700

    Clean up

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,7 +16,7 @@ import './tasks/mapGen';
 // to get the file size of each smart contract, run:
 // yarn run hardhat size-contracts
 
-const { USER1_PK, USER2_PK, LOCALHOST_USER1_PK, LOCALHOST_USER2_PK, CONSTELLATION_RPC_URL, TAILSCALE_MAIN, ALTLAYER_RPC_URL, CONSTELLATIONNEW_RPC_URL, EXFAC_RPC_URL } = process.env;
+const { USER1_PK, USER2_PK, LOCALHOST_USER1_PK, LOCALHOST_USER2_PK, CONSTELLATION_RPC_URL, TAILSCALE_MAIN, ALTLAYER_RPC_URL, EXFAC_RPC_URL } = process.env;
 
 export default {
   defaultNetwork: 'localhost',
@@ -45,11 +45,6 @@ export default {
     constellation: {
       url: CONSTELLATION_RPC_URL,
       accounts: [USER1_PK, USER2_PK],
-      chainId: 2901,
-    },
-    constellationNew: {
-      url: CONSTELLATIONNEW_RPC_URL,
-      accounts: [LOCALHOST_USER1_PK, LOCALHOST_USER2_PK],
       chainId: 2938,
     },
     altlayer: {
