55c7deace045aee3ae6e3f4d5603f8499e0333bb
ModeoC <63042678+ModeoC@users.noreply.github.com>
2023-01-04 11:10:25 -0800

    handshake deal complete

--- contracts/treaties/HandShakeDeal.sol
+++ contracts/treaties/HandShakeDeal.sol
@@ -19,8 +19,6 @@ contract HandshakeDeal is CurioTreaty {
         approveStartTroopProduction,
         approveEndTroopProduction,
         approveMove,
-        approveOrganizeArmy,
-        approveDisbandArmy,
         approveStartGather,
         approveEndGather,
         approveUnloadResources,
@@ -97,27 +95,6 @@ contract HandshakeDeal is CurioTreaty {
         super.treatyLeave();
     }
 
-
-    function approveBattle(uint256 _nationID, bytes memory _encodedParams) public view override returns (bool) {
-        // Disapprove if target nation is part of collective fund
-        (, uint256 armyID, uint256 battleTargetID) = abi.decode(_encodedParams, (uint256, uint256, uint256));
-
-        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
-        for (uint256 i = 0; i < signedDealIDs.length; i++) {
-            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
-            if (deal.functionOfAgreement == ApprovalFunctionType.approveBattle) {
-                (uint256 agreedArmyID, uint256 agreedBattleTargetID) = abi.decode(deal.encodedParams, (uint256, uint256));
-                if (agreedArmyID == armyID && agreedBattleTargetID == battleTargetID) {
-                    if (block.timestamp < deal.timeLock) {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        return super.approveBattle(_nationID, _encodedParams);
-    }
-
     function approveUpgradeCapital(uint256 _nationID, bytes memory _encodedParams) public view override returns (bool) {
         (, uint256 capitalID) = abi.decode(_encodedParams, (uint256, uint256));
         uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
@@ -133,5 +110,261 @@ contract HandshakeDeal is CurioTreaty {
             }
         }
         return super.approveUpgradeCapital(_nationID, _encodedParams);
+    }
+
+    function approveMoveCapital(uint256 _nationID, bytes memory _encodedParams) public view override returns (bool) {
+        (, uint256 capitalID, uint256 targetTileID) = abi.decode(_encodedParams, (uint256, uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveMoveCapital) {
+                (uint256 agreedCapitalID, uint256 agreedTargetTileID) = abi.decode(deal.encodedParams, (uint256, uint256));
+                if (capitalID == agreedCapitalID && targetTileID == agreedTargetTileID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveMoveCapital(_nationID, _encodedParams);
+    }
+
+    function approveClaimTile(uint256 _nationID, bytes memory _encodedParams) public view override returns (bool) {
+        (, uint256 tileID) = abi.decode(_encodedParams, (uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveClaimTile) {
+                uint256 agreedTileID = abi.decode(deal.encodedParams, (uint256));
+                if (tileID == agreedTileID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveClaimTile(_nationID, _encodedParams);
+    }
+
+    function approveUpgradeTile(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 tileID) = abi.decode(_encodedParams, (uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveUpgradeTile) {
+                uint256 agreedTileID = abi.decode(deal.encodedParams, (uint256));
+                if (tileID == agreedTileID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveUpgradeTile(_nationID, _encodedParams);
+    }
+
+    function approveRecoverTile(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 tileID) = abi.decode(_encodedParams, (uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveRecoverTile) {
+                uint256 agreedTileID = abi.decode(deal.encodedParams, (uint256));
+                if (tileID == agreedTileID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveRecoverTile(_nationID, _encodedParams);
+    }
+
+    function approveDisownTile(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 tileID) = abi.decode(_encodedParams, (uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveDisownTile) {
+                uint256 agreedTileID = abi.decode(deal.encodedParams, (uint256));
+                if (tileID == agreedTileID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveDisownTile(_nationID, _encodedParams);
+    }
+
+    function approveStartTroopProduction(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 capitalID, uint256 templateID, uint256 amount) = abi.decode(_encodedParams, (uint256, uint256, uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveStartTroopProduction) {
+                (uint256 agreedCapitalID, uint256 agreedTemplateID, uint256 agreedAmount) = abi.decode(deal.encodedParams, (uint256, uint256, uint256));
+                if (capitalID == agreedCapitalID && templateID == agreedTemplateID && amount == agreedAmount) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveStartTroopProduction(_nationID, _encodedParams);
+    }
+
+    function approveEndTroopProduction(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 capitalID) = abi.decode(_encodedParams, (uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveEndTroopProduction) {
+                (uint256 agreedCapitalID) = abi.decode(deal.encodedParams, uint256);
+                if (capitalID == agreedCapitalID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveEndTroopProduction(_nationID, _encodedParams);
+    }
+
+    function approveMove(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 armyID, Position memory targetPosition) = abi.decode(_encodedParams, (uint256, uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveMove) {
+                (uint256 agreedArmyID, Position memory  agreedTargetPosition) = abi.decode(deal.encodedParams, (uint256, Position));
+                if (agreedArmyID == armyID && GameLib.coincident(targetPosition, agreedTargetPosition)) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveMove(_nationID, _encodedParams);
+    }
+
+    function approveBattle(uint256 _nationID, bytes memory _encodedParams) public view override returns (bool) {
+        (, uint256 armyID, uint256 battleTargetID) = abi.decode(_encodedParams, (uint256, uint256, uint256));
+
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveBattle) {
+                (uint256 agreedArmyID, uint256 agreedBattleTargetID) = abi.decode(deal.encodedParams, (uint256, uint256));
+                if (agreedArmyID == armyID && agreedBattleTargetID == battleTargetID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveBattle(_nationID, _encodedParams);
+    }
+
+    function approveStartGather(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 armyID, uint256 resourceID) = abi.decode(_encodedParams, (uint256, uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveStartGather) {
+                (uint256 agreedArmyID, uint256 agreedResourceID) = abi.decode(deal.encodedParams, (uint256, uint256));
+                if (agreedArmyID == armyID && agreedResourceID == resourceID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveStartGather(_nationID, _encodedParams);
+    }
+
+    function approveEndGather(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 armyID) = abi.decode(_encodedParams, (uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveEndGather) {
+                (uint256 agreedArmyID) = abi.decode(deal.encodedParams, uint256);
+                if (agreedArmyID == armyID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveEndGather(_nationID, _encodedParams);
+    }
+
+    function approveUnloadResources(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 armyID) = abi.decode(_encodedParams, (uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveEndGather) {
+                (uint256 agreedArmyID) = abi.decode(deal.encodedParams, uint256);
+                if (agreedArmyID == armyID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveUnloadResources(_nationID, _encodedParams);
+    }
+
+    function approveHarvestResource(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 resourceID) = abi.decode(_encodedParams, (uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveHarvestResource) {
+                uint256 agreedResourceID = abi.decode(deal.encodedParams, uint256);
+                if (agreedResourceID == resourceID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveHarvestResource(_nationID, _encodedParams);
+    }
+
+    function approveHarvestResourcesFromCapital(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 capitalID) = abi.decode(_encodedParams, (uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveHarvestResourcesFromCapital) {
+                uint256 agreedCapitalID = abi.decode(deal.encodedParams, uint256);
+                if (agreedCapitalID == capitalID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveHarvestResourcesFromCapital(_nationID, _encodedParams);
+    }
+
+    function approveUpgradeResource(_nationID, _encodedParams) public view override returns (bool) {
+        (, uint256 resourceID) = abi.decode(_encodedParams, (uint256, uint256));
+        uint256[] memory signedDealIDs = nationIDToDealIDs[_nationID];
+        for (uint256 i = 0; i < signedDealIDs.length; i++) {
+            Deal memory deal = dealIDToDeal[signedDealIDs[i]];
+            if (deal.functionOfAgreement == ApprovalFunctionType.approveUpgradeResource) {
+                uint256 agreedResourceID = abi.decode(deal.encodedParams, uint256);
+                if (agreedResourceID == resourceID) {
+                    if (block.timestamp < deal.timeLock) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return super.approveUpgradeResource(_nationID, _encodedParams);
     }
 }
