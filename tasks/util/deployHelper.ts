5d01371fae062dd1146882691bca184609c0bc8f
Yijia-Chen <yijiachenmain@gmail.com>
2023-01-21 15:34:30 -0800

    feat: add mercenary league

--- tasks/util/deployHelper.ts
+++ tasks/util/deployHelper.ts
@@ -226,7 +226,7 @@ export const initializeGame = async (hre: HardhatRuntimeEnvironment, worldConsta
 
   // Deploy treaty templates
   startTime = performance.now();
-  const treatyTemplateNames = ['Alliance', 'NonAggressionPact', 'Embargo', 'CollectiveDefenseFund', 'SimpleOTC', 'HandshakeDeal'];
+  const treatyTemplateNames = ['Alliance', 'NonAggressionPact', 'Embargo', 'CollectiveDefenseFund', 'SimpleOTC', 'HandshakeDeal', 'MercenaryLeague'];
   for (const name of treatyTemplateNames) {
     await deployTreatyTemplate(name, admin, hre, diamond, gasLimit);
   }
