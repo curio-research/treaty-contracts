3112000042e92eb9ca1ff9b5df3488bcab4252ce
Kevin Z <zkw917@gmail.com>
2023-04-02 21:19:17 -0700

    Fixing

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,8 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+// import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
-contract GameItem is ERC721 {
-    constructor() public ERC721("Treaty", "TREATY") {}
+// contract GameItem is ERC721 {
+//     constructor() public ERC721("Treaty", "TREATY") {}
+// }
+
+contract Hello {
+
 }
