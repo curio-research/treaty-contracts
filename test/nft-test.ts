4c703eef51e92db5c44540f7da5c9a098f107f26
Kyles Light <kuilin@yangqianguan.com>
2023-04-08 13:42:16 +0800

    feat: change in abi

--- test/nft-test.ts
+++ test/nft-test.ts
@@ -1,8 +1,7 @@
 import fs from 'fs';
 import { expect } from 'chai';
 import { providers, Contract, Wallet } from 'ethers';
-import { L1NFT__factory, L1NFT_ABI } from './../typechain-types/factories/L1NFT__factory';
-import { L2NFT__factory, L2NFT_ABI } from './../typechain-types/factories/L2NFT__factory';
+import { artifacts } from 'hardhat';
 import { L1NFT } from './../typechain-types/L1NFT';
 import { L2NFT } from './../typechain-types/L2NFT';
 
@@ -29,14 +28,17 @@ describe('Mint', () => {
   it('Should change ownership from Null to Account 1', async function () {
     const myStructFromFile: NFTPair = JSON.parse(fs.readFileSync('./RecentNFTPair.json', 'utf-8'));
 
+    const L1NFT_ABI = await artifacts.readArtifact('L1NFT');
+    const L2NFT_ABI = await artifacts.readArtifact('L2NFT');
+
     const L1Provider = new providers.JsonRpcProvider(L1RPC);
     const L2Provider = new providers.JsonRpcProvider(L2RPC);
 
     const L1DeployerSigner = new Wallet(process.env.ADMIN_PK || '', L1Provider);
     const L2DeployerSigner = new Wallet(process.env.ADMIN_PK || '', L2Provider);
 
-    const L1NFT = new Contract(myStructFromFile.L1NFT, L1NFT_ABI, L1DeployerSigner) as L1NFT;
-    const L2NFT = new Contract(myStructFromFile.L2NFT, L2NFT_ABI, L2Provider) as L2NFT;
+    const L1NFT = new Contract(myStructFromFile.L1NFT, L1NFT_ABI.abi, L1DeployerSigner) as L1NFT;
+    const L2NFT = new Contract(myStructFromFile.L2NFT, L2NFT_ABI.abi, L2Provider) as L2NFT;
 
     await L1NFT.mint(1);
 
@@ -51,14 +53,17 @@ describe('Mint', () => {
   it('Should change ownership from Null to Account 2', async function () {
     const myStructFromFile: NFTPair = JSON.parse(fs.readFileSync('./RecentNFTPair.json', 'utf-8'));
 
+    const L1NFT_ABI = await artifacts.readArtifact('L1NFT');
+    const L2NFT_ABI = await artifacts.readArtifact('L2NFT');
+
     const L1Provider = new providers.JsonRpcProvider(L1RPC);
     const L2Provider = new providers.JsonRpcProvider(L2RPC);
 
     const L1DeployerSigner = new Wallet(process.env.ADMIN_PK || '', L1Provider);
     const L2DeployerSigner = new Wallet(process.env.ADMIN_PK || '', L2Provider);
 
-    const L1NFT = new Contract(myStructFromFile.L1NFT, L1NFT_ABI, L1DeployerSigner) as L1NFT;
-    const L2NFT = new Contract(myStructFromFile.L2NFT, L2NFT_ABI, L2DeployerSigner) as L2NFT;
+    const L1NFT = new Contract(myStructFromFile.L1NFT, L1NFT_ABI.abi, L1DeployerSigner) as L1NFT;
+    const L2NFT = new Contract(myStructFromFile.L2NFT, L2NFT_ABI.abi, L2DeployerSigner) as L2NFT;
 
     // mint to account 1
     await L1NFT.mint(1);
@@ -77,14 +82,17 @@ describe('Mint', () => {
   it('Should change ownership from Null to Account 3', async function () {
     const myStructFromFile: NFTPair = JSON.parse(fs.readFileSync('./RecentNFTPair.json', 'utf-8'));
 
+    const L1NFT_ABI = await artifacts.readArtifact('L1NFT');
+    const L2NFT_ABI = await artifacts.readArtifact('L2NFT');
+
     const L1Provider = new providers.JsonRpcProvider(L1RPC);
     const L2Provider = new providers.JsonRpcProvider(L2RPC);
 
     const L1DeployerSigner = new Wallet(process.env.ADMIN_PK || '', L1Provider);
     const L2DeployerSigner = new Wallet(process.env.ADMIN_PK || '', L2Provider);
 
-    const L1NFT = new Contract(myStructFromFile.L1NFT, L1NFT_ABI, L1DeployerSigner) as L1NFT;
-    const L2NFT = new Contract(myStructFromFile.L2NFT, L2NFT_ABI, L2DeployerSigner) as L2NFT;
+    const L1NFT = new Contract(myStructFromFile.L1NFT, L1NFT_ABI.abi, L1DeployerSigner) as L1NFT;
+    const L2NFT = new Contract(myStructFromFile.L2NFT, L2NFT_ABI.abi, L2DeployerSigner) as L2NFT;
 
     // mint to account 1
     await L1NFT.mint(1);
@@ -103,14 +111,17 @@ describe('Mint', () => {
   it('Every token ownership should be equal from L1 to L2', async function () {
     const myStructFromFile: NFTPair = JSON.parse(fs.readFileSync('./RecentNFTPair.json', 'utf-8'));
 
+    const L1NFT_ABI = await artifacts.readArtifact('L1NFT');
+    const L2NFT_ABI = await artifacts.readArtifact('L2NFT');
+
     const L1Provider = new providers.JsonRpcProvider(L1RPC);
     const L2Provider = new providers.JsonRpcProvider(L2RPC);
 
     const L1DeployerSigner = new Wallet(process.env.ADMIN_PK || '', L1Provider);
     const L2DeployerSigner = new Wallet(process.env.ADMIN_PK || '', L2Provider);
 
-    const L1NFT = new Contract(myStructFromFile.L1NFT, L1NFT_ABI, L1DeployerSigner) as L1NFT;
-    const L2NFT = new Contract(myStructFromFile.L2NFT, L2NFT_ABI, L2DeployerSigner) as L2NFT;
+    const L1NFT = new Contract(myStructFromFile.L1NFT, L1NFT_ABI.abi, L1DeployerSigner) as L1NFT;
+    const L2NFT = new Contract(myStructFromFile.L2NFT, L2NFT_ABI.abi, L2DeployerSigner) as L2NFT;
 
     const tokenLen = (await L1NFT._currentIndex()).toNumber();
 
