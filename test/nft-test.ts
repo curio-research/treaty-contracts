fae5b3f88726c57917d89da784b6c909bc0cbc39
Kyles Light <kuilin@yangqianguan.com>
2023-04-13 13:50:01 +0800

    feat: clean up test

--- test/nft-test.ts
+++ test/nft-test.ts
@@ -2,7 +2,6 @@ import fs from 'fs';
 import { expect } from 'chai';
 import { providers, Contract, Wallet } from 'ethers';
 import { artifacts } from 'hardhat';
-import { Alchemy, Network } from 'alchemy-sdk';
 import { L1NFT, TransferEvent } from './../typechain-types/L1NFT';
 import { L2NFT } from './../typechain-types/L2NFT';
 
@@ -11,25 +10,10 @@ interface NFTPair {
   L2NFT: string;
 }
 
-const L1RPC = 'https://eth-mainnet.g.alchemy.com/v2/D3yJ1cYWPCJX4WE2vATcUcyBTaAgEy-A';
-const L2RPC = 'https://curiov3.constellationchain.xyz/http';
-const Account1 = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
-const Account2 = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
-const Account3 = '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC';
+const { CONSTELLATION_RPC_URL, ALCHEMY_RPC_URL, NFT_BLOCK_NUMBER_L1 } = process.env;
 
-const config = {
-  apiKey: 'D3yJ1cYWPCJX4WE2vATcUcyBTaAgEy-A',
-  network: Network.ETH_MAINNET,
-};
-const alchemy = new Alchemy(config);
-
-const sleep = (t: number = 1000): Promise<void> => {
-  return new Promise((r) => {
-    setTimeout(() => {
-      r();
-    }, t);
-  });
-};
+const L1RPC = ALCHEMY_RPC_URL;
+const L2RPC = CONSTELLATION_RPC_URL;
 
 describe('Remote L1 to L2', () => {
   it('Every token ownership should be equal from L1 to L2', async function () {
@@ -47,9 +31,9 @@ describe('Remote L1 to L2', () => {
     const L1NFT = new Contract(myStructFromFile.L1NFT, L1NFT_ABI.abi, L1DeployerSigner) as L1NFT;
     const L2NFT = new Contract(myStructFromFile.L2NFT, L2NFT_ABI.abi, L2DeployerSigner) as L2NFT;
 
-    const BlockBatchSize = 6000;
+    const BlockBatchSize = 500;
 
-    const fromBlock = 17020519;
+    const fromBlock = +(NFT_BLOCK_NUMBER_L1 || '');
     const blockNumber = await L1Provider.getBlockNumber();
     const toBlock = blockNumber;
     let events: Array<TransferEvent> = [];
@@ -105,22 +89,3 @@ describe('Remote L1 to L2', () => {
     }
   });
 });
-
-// describe('Temp test', () => {
-//   it('Every token ownership should be equal from L1 to L2', async function () {
-//     const myStructFromFile: NFTPair = JSON.parse(fs.readFileSync('./RecentNFTPair.json', 'utf-8'));
-
-//     const L2NFT_ABI = await artifacts.readArtifact('L2NFT');
-
-//     const L2Provider = new providers.JsonRpcProvider(L2RPC);
-
-//     const L2DeployerSigner = new Wallet(process.env.ADMIN_PK || '', L2Provider);
-
-//     // const L1NFT = new Contract(myStructFromFile.L1NFT, L1NFT_ABI.abi, L1DeployerSigner) as L1NFT;
-//     const L2NFT = new Contract(myStructFromFile.L2NFT, L2NFT_ABI.abi, L2DeployerSigner) as L2NFT;
-
-//     const L2TokenOwnerAddress = await L2NFT.ownerOf(148);
-
-//     console.log('-> L2TokenOwnerAddress', L2TokenOwnerAddress);
-//   });
-// });
